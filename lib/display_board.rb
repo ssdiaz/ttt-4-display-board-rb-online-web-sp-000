f8599e5f1a49f2ecd418010d374d16405a547311
Sam D <samidiaz3@gmail.com>
2021-01-04 02:03:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board_empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 board_X = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
 
 def display_board (board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[0]}  | #{board[1]} | #{board[2]}"
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts "-----------"
